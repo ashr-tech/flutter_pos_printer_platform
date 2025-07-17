3b6d104b90d8d3c8f734860f7869c28dbc4fdb0f
elrizwiraswara <elrizwiraswara@gmail.com>
2025-07-17 14:44:15 +0700

    update image and some fixes

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/yasfdany/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/yasfdany/Documents/FlutterProjects/dds_inv_pos/packages/flutter_pos_printer_platform-1.4.2/example"
+export "FLUTTER_ROOT=D:\Softwares\Developments\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Companies\Retail DDS Singapore\packages\flutter_pos_printer_platform\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
